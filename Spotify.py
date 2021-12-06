8b03bb6a75d23068dafbe949d63aaab18191e66f
Nikita Sergievsky <ReetRay@protonmail.com>
2021-12-06 19:09:34 +0300

    Избавился от LastFMApi

--- Spotify.py
+++ Spotify.py
@@ -5,10 +5,14 @@ from librespot.audio.decoders import AudioQuality, VorbisOnlyAudioQuality
 from librespot.proto.Metadata_pb2 import Album
 
 from pydub import AudioSegment
-import os
+import os, sys
+import syncmm
+import time
 
 from edit_song import MetaData
-from lastfm import AlbumCover
+#from lastfm import AlbumCover
+
+import requests
 
 LOGIN = ""
 PASSWORD = ""
@@ -16,23 +20,33 @@ LAST_FM_API_KEY = ""
 
 class Downloader:
 
-    def __init__(self, login: str, password: str, last_fm_api_key: str):
-        self.__session = Session.Builder().user_pass(LOGIN, PASSWORD).create()
-        self.__LAST_FM_API_KEY = last_fm_api_key
+    def __init__(self, login: str = None, password: str = None, session: Session = None):
+        if session:
+            self.__session = session
+        else:
+            self.__session = Session.Builder().user_pass(login, password).create()
+        #self.__session = Session.Builder().user_pass(LOGIN, PASSWORD).create()
     
-    def __link_parse(self, link: str):
-        start = link.find("track/")
+    def _link_parse(self, link: str):
+        link_type = None
+        if "track" in link:
+            link_type = "track"
+        elif "album" in link:
+            link_type = "album"
+        elif "episode" in link:
+            link_type = "episode"
+ 
+        start = link.find(link_type + "/")
         if start == -1:
             return None
         end = link.find("?si")
-        return "spotify:track:{}".format(link[start+6:end]) # 6 это смещение по тегу
+        return "spotify:{}:{}".format(link_type, link[start+6:end]) # 6 это смещение по тегу
 
     def __download_from_stream(self, stream) -> bytes:
-        b = bytes('',encoding='utf-8')
+        b = bytes('', encoding='utf-8')
         size = stream.input_stream.stream().size()
         for _ in range(size):
             b += stream.input_stream.stream().read()
-
         return b
     
     def __decompile_cache_to_mp3(self, title: str):
@@ -62,36 +76,65 @@ class Downloader:
         )
         return artist, album, title, track_num, release_date
     
-    def __download_album_cover(self, artist: str, album: str):
-        a = AlbumCover(self.__LAST_FM_API_KEY)
-        cover_in_bytes = a.get_album_cover(artist, album)
+    def __download_album_cover(self, url: str):
+        r = requests.get(url)
         with open("cover.png", "wb") as f:
-            f.write(cover_in_bytes)
+            f.write(r.content)
     
     def __set_song_metadata(self, artist: str, album: str, title: str, track_num: int, release_date: str, path_to_cover: str):
         m = MetaData(title + ".mp3")
         m.change(artist, album, title, track_num, release_date)
         m.set_album_cover(path_to_cover)
 
-    def download_song(self, link: str):
+    def download_track(self, uri_link: str, folder_to_save: str, url_to_cover: str):
         """
             link must be "spotify:track:0Y9D1Bc4fAkeZnVsGKdij6"
-            or "https://open.spotify.com/track/2URDbWGmPz3vhagl25p8OC?si=15ee14d7e038406f"
         """
-        if link.startswith("https://"):
-            link = self.__link_parse(link)
 
-        track_id = TrackId.from_uri(link)
-        stream = self.__session.content_feeder().load(track_id, VorbisOnlyAudioQuality(AudioQuality.VERY_HIGH), False, None)
+        track_id = TrackId.from_uri(uri_link)
+        try:
+            stream = self.__session.content_feeder().load(track_id, VorbisOnlyAudioQuality(AudioQuality.VERY_HIGH), False, None)
+        except:
+            print("Трек недоступен", link)
+            return
+
         with open("cached_song","wb") as f:
             f.write(self.__download_from_stream(stream))
 
         artist, album, title, track_num, release_date = self.__get_song_metadata(stream.track)
         self.__decompile_cache_to_mp3(title)
-        self.__download_album_cover(artist, album)
+        self.__download_album_cover(url_to_cover)
 
         self.__set_song_metadata(artist, album, title, track_num, release_date, "cover.png")
+        os.replace(title + ".mp3", os.path.join(folder_to_save, title + ".mp3"))
+
+
+class Main:
+
+    __parser_spotify = None
+    __downloader = None
+
+    def __init__(self, login: str, password: str): # TODO: token auth
+        self.__parser_spotify = syncmm.spotify(login, password)
+        self.__downloader = Downloader(session=self.__parser_spotify._session)
+
+    def download_by_link(self, link: str, path_to_folder: str):
+        uri = self.__downloader._link_parse(link)
+        print(uri)
+        song = self.__parser_spotify._get_a_track_by_uri(uri)
+        print(f"Downloading song: {song['artists']} - {song['album']} - {song['title']}")
+        self.__downloader.download_track(song['uri'], path_to_folder, song['cover_url'])
+        print(f"Downloaded!")
+
+    def download_all_liked_tracks(self, path_to_folder: str):
+        liked = self.__parser_spotify.liked()
+        if not os.access(path_to_folder, os.R_OK):
+            os.mkdir(path_to_folder)
+        
+        for i,song in enumerate(liked):
+            self.__downloader.download_song(song['uri'], path_to_folder, song['cover_url'])
+            print(f"Downloaded {i+1}) {song['artists'][0]} - {song['title']}")
 
 if __name__ == "__main__":
-    d = Downloader(LOGIN, PASSWORD, LAST_FM_API_KEY)
-    d.download_song("https://open.spotify.com/track/2URDbWGmPz3vhagl25p8OC?si=6a4c3c70c074475e")
+    m = Main(LOGIN, PASSWORD)
+    m.download_by_link(sys.argv[1], sys.argv[2])
