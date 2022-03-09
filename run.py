001caf9f7c3f48900668d01d3dc0ab72b2dcbff7
Sergievsky Nikita <ReetRay@protonmail.com>
2022-03-09 23:19:21 +0300

    Добавил создание пустой папки

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from client import RRSpotify
+import os
 
 if __name__ == "__main__":
+
+    try:
+        os.mkdir("data")
+    except:
+        pass
+
     login = input("Enter Spotfiy login: ")
     password = input("Enter Spotify password: ")
     core = RRSpotify(login, password)
